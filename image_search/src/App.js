a34c7af78f19c1dff255243c0b3f08b5475dccc1
sobhiEgbaria <sobhim686@gmail.com>
2023-12-13 14:01:06 +0200

    starting with image_search app

---
+++ image_search/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>img store</h1>
+    </>
+  );
+}
+
+export default App;
--- section-7-PicsProject/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization: "Client-ID 5LLT5i5QEzlQIwQ08fTzcCOpMcbiwnuQWAeUYxQl35w",
-  },
-});
