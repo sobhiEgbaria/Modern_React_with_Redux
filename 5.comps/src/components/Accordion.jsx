7323265816250dbdadd0bbb3197b0894007d8423
sobhiEgbaria <sobhim686@gmail.com>
2025-05-15 14:36:06 +0300

    add content and lable

--- 5.comps/src/components/Accordion.jsx
+++ 5.comps/src/components/Accordion.jsx
@@ -3,16 +3,19 @@ import React from "react";
 const Accordion = () => {
   const items = [
     {
+      id: 1,
       label: "Can I use React on a project?",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
     },
     {
+      id: 2,
       label: "Can I use Javascript on a project?",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
     },
     {
+      id: 3,
       label: "Can I use CSS on a project?",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
@@ -20,7 +23,14 @@ const Accordion = () => {
   ];
   return (
     <div>
-      <p>{items[0].content}</p>
+      {items.map((item) => {
+        return (
+          <div key={item.id}>
+            <div>{item.label}</div>
+            <div>{item.content}</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
