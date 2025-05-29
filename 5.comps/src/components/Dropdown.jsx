c8d1826a3ddd135cc1a2a7ab54de92486df341fe
sobhiEgbaria <sobhim686@gmail.com>
2025-05-29 14:58:16 +0300

    finsh dropdown com

--- 5.comps/src/components/Dropdown.jsx
+++ 5.comps/src/components/Dropdown.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, value, onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handelClick = () => {
+    setIsOpen(!isOpen);
+  };
+  const handelOptionClick = (option) => {
+    setIsOpen(!isOpen);
+    onChange(option);
+  };
+
+  const renderOptions = options.map((option) => {
+    return (
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1 flex justify-between"
+        key={option.id}
+        onClick={() => handelOptionClick(option)}
+      >
+        {option.label} <GoChevronLeft className="text-lg" />
+      </div>
+    );
+  });
+
   return (
-    <>
-      {options.map((option) => {
-        return <div key={option.value}>{option.label}</div>;
-      })}
-    </>
+    <div className="w-48 relative ">
+      <div
+        className="flex justify-between item-center cursor-pointer rounded p-3 shadow bg-white w-full"
+        onClick={handelClick}
+      >
+        {value}
+        <GoChevronDown className="text-lg" />
+      </div>
+
+      {isOpen ? (
+        <div className="absolute top-full rounded p-3 shadow bg-white w-full">
+          {renderOptions}
+        </div>
+      ) : null}
+    </div>
   );
 };
 
--- 5.comps/src/pages/DropdownPge.jsx
+++ 5.comps/src/pages/DropdownPge.jsx
@@ -1,22 +1,38 @@
 import Dropdown from "../components/Dropdown";
+import { useState } from "react";
+
 const options = [
   {
+    id: 1,
     label: "Red",
     value: "Red",
   },
   {
+    id: 3,
     label: "green",
     value: "green",
   },
   {
+    id: 2,
     label: "white",
     value: "white",
   },
 ];
+
 const DropdownPage = () => {
+  const [selectValue, setSelectValue] = useState("select");
+
+  const handelSelectValue = (option) => {
+    setSelectValue(option.label);
+  };
+
   return (
     <>
-      <Dropdown options={options} />
+      <Dropdown
+        options={options}
+        value={selectValue}
+        onChange={handelSelectValue}
+      />
     </>
   );
 };
