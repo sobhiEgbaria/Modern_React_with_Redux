fba0bb97617774a91627170bb696c0ea017ef0dd
sobhiEgbaria <sobhim686@gmail.com>
2025-06-18 23:52:18 +0300

    use reducer

--- 5.comps/src/pages/Counter_reducer.jsx
+++ 5.comps/src/pages/Counter_reducer.jsx
@@ -1,21 +1,76 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import Button from "../components/Button";
+import Panel from "../css_components/Panel";
+
+const reducer = (state, action) => {
+  if (action.type == "increment-counter") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type == "decrement-counter") {
+    return {
+      ...state,
+      counter: state.counter - 1,
+    };
+  }
+
+  if (action.type == "change-value") {
+    return {
+      ...state,
+      valueToAdd: action.payload,
+    };
+  }
+
+  if (action.type == "add-value") {
+    return {
+      ...state,
+      counter: state.counter + state.valueToAdd,
+      valueToAdd: 0,
+    };
+  }
+};
 
 const Counter_reducer = ({ initialCount }) => {
-  const [counter, setCounter] = useState(initialCount);
+  // const [counter, setCounter] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState("");
+
+  const [state, dispatch] = useReducer(reducer, {
+    counter: initialCount,
+    valueToAdd: 0,
+  });
 
   const incrementHandler = () => {
-    setCounter(counter + 1);
+    dispatch({
+      type: "increment-counter",
+    });
   };
 
   const decrementHandler = () => {
-    setCounter(counter - 1);
+    dispatch({
+      type: "decrement-counter",
+    });
+  };
+  const onChangeHandler = (event) => {
+    const value = parseInt(event.target.value) || 0;
+    dispatch({
+      type: "change-value",
+      payload: value,
+    });
+  };
+  const addValueHandler = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: "add-value",
+    });
   };
 
   return (
-    <>
-      <h1>counter: {counter}</h1>
-      <div className="flex">
+    <Panel className="m-3 w-sm">
+      <h1>counter: {state.counter}</h1>
+      <div className="flex flex-row">
         <Button onClick={incrementHandler} primary>
           increment
         </Button>
@@ -23,7 +78,19 @@ const Counter_reducer = ({ initialCount }) => {
           decrement
         </Button>
       </div>
-    </>
+      <form>
+        <label>Add a lot: </label>
+        <input
+          value={state.valueToAdd || ""}
+          onChange={onChangeHandler}
+          type="number"
+          className="border p-1 m-2 bg-gray-50 border-gray-300"
+        />
+        <Button onClick={addValueHandler} success>
+          Add it!!
+        </Button>
+      </form>
+    </Panel>
   );
 };
 
