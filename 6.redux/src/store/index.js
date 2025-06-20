2a39f4b4b051aae413b675b69cbffd13163a7750
sobhiEgbaria <sobhim686@gmail.com>
2025-06-20 14:53:00 +0300

    redux

---
+++ 6.redux/src/store/index.js
@@ -0,0 +1,22 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const songSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+
+    removeSong(state, action) {},
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    songs: songSlice.reducer,
+  },
+});
+
+export { store };
+export const { addSong } = songSlice.actions;
